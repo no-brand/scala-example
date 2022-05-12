37bb051ee688cb67ff4a091d8b8df1b5f26aae87
no-brand <do.dream.david@gmail.com>
2022-05-13 07:49:25 +0900

    feat(command-arg): pattern-matching

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,19 @@
 object Main extends App {
-  args.foreach(arg => println(arg))
+
+  def nextArgs(map: Map[String, Any], list: List[String]): Map[String, Any] = {
+    list match {
+      case Nil => map
+      case "--arg1" :: v :: rest => nextArgs(map ++ Map("arg1" -> v.toInt), rest)
+      case "--arg2" :: v :: rest => nextArgs(map ++ Map("arg2" -> v.toInt), rest)
+      case _ :: rest => nextArgs(map, rest)
+    }
+  }
+
+  val options = nextArgs(Map(), args.toList)
+  if (args.length == 0 || options.size != 2) {
+    println("\nUsage: command [--arg1 num] [--arg2 num]\n")
+  }
+  else {
+    println(options)
+  }
 }
